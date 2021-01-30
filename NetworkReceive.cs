a6f68d08da309d9b99b9b5add5ce9dc81181d75b
yagox10 <yagocgomes3@yahoo.com.br>
2021-01-29 23:30:39 -0300

    receive/send data

---
+++ NetworkReceive.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KaymakNetwork;
+
+namespace NewServer
+{
+    enum ClientPackets
+    {
+        CPing = 1,
+    }
+    internal static class NetworkReceive
+    {
+        internal static void PacketRouter()
+        {
+            NetworkConfig.socket.PacketId[(int)ClientPackets.CPing] = Packet_Ping;
+        }
+
+        private static void Packet_Ping(int connectionID, ref byte[] data)
+        {
+            ByteBuffer buffer = new ByteBuffer(data);
+            string msg = buffer.ReadString();
+
+            Console.WriteLine(msg);
+
+            buffer.Dispose();
+        }
+
+    }
+}
---
+++ NetworkSend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using KaymakNetwork;
+
+namespace NewServer
+{
+    enum ServerPackets
+    {
+        SWelcomeMsg = 1,
+    }
+
+    internal static class NetworkSend
+    {
+        public static void WelcomeMsg(int connectionID, string msg)
+        {
+            ByteBuffer buffer = new ByteBuffer(4);
+            buffer.WriteInt32((int)ServerPackets.SWelcomeMsg);
+            buffer.WriteString(msg);
+            NetworkConfig.socket.SendDataTo(connectionID, buffer.Data, buffer.Head);
+
+            buffer.Dispose();
+        }
+    }
+}
